22c925e96f85729d6447d650f17afefae3782a28
superstan777 <viir4d@gmail.com>
2025-11-24 16:45:53 +0100

    refactor: update sidebar tags enpoints to get user from cookie

--- src/sidebar-tags/sidebar-tags.controller.ts
+++ src/sidebar-tags/sidebar-tags.controller.ts
@@ -1,23 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { SidebarTagsService } from './sidebar-tags.service';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import type { Request } from 'express';
 
 @Controller('sidebar-tags')
 export class SidebarTagsController {
   constructor(private readonly sidebarTagsService: SidebarTagsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('meal-type')
   async getByMealType(
     @Body('meal_type_id') meal_type_id: number,
-    @Body('user_id') user_id: number,
+    @Req() req: Request,
   ) {
-    return this.sidebarTagsService.findByMealType(meal_type_id, user_id);
+    const user = req.user as { id: number };
+    return this.sidebarTagsService.findByMealType(meal_type_id, user.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async createTag(
-    @Body() body: { user_id: number; meal_type_id: number; tag_name: string },
+    @Body() body: { meal_type_id: number; tag_name: string },
+    @Req() req: Request,
   ) {
-    const { user_id, meal_type_id, tag_name } = body;
-    return this.sidebarTagsService.createTag(user_id, meal_type_id, tag_name);
+    const user = req.user as { id: number };
+    const { meal_type_id, tag_name } = body;
+    return this.sidebarTagsService.createTag(user.id, meal_type_id, tag_name);
   }
 }
